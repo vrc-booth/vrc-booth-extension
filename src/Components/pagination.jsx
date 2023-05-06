4e17935f15b61791f2745f366ed3ec5351e798e0
ribeKim <ribe@ribe.io>
2023-05-07 02:18:33 +0900

    Pagination 임시 완성

--- src/Components/pagination.jsx
+++ src/Components/pagination.jsx
@@ -1,5 +1,21 @@
+function Pagination ({ data, page, onButtonClick }) {
 
-function Pagination() {
+  let totalPage = Math.ceil(data?.count / page?.pageSize)
+  const buttonRender = () => {
+    const arr = []
+    for (let i = 0; i < totalPage; i++) {
+      arr.push(i + 1)
+    }
+    return arr
+  }
+
+  const buttonClick = (el) => {
+    onButtonClick({
+      current: el,
+      pageSize: 5
+    })
+  }
+
   const notSelectedPage =
     `tw-relative tw-inline-flex tw-items-center tw-px-4 tw-py-2 tw-text-sm tw-font-semibold tw-text-gray-900 tw-ring-1
      tw-ring-inset tw-ring-gray-300 hover:tw-bg-secondary focus:tw-z-20 focus:tw-outline-offset-0 tw-cursor-pointer`
@@ -10,35 +26,39 @@ function Pagination() {
 
   return (
     <>
-      <div className="tw-flex tw-items-center tw-justify-between tw-border-t tw-border-gray-200 tw-bg-white tw-px-4 tw-py-3">
+      <div
+        className="tw-flex tw-items-center tw-justify-between tw-border-t tw-border-gray-200 tw-bg-white tw-px-4 tw-py-3">
         <div className="tw-flex tw-flex-1 tw-items-center tw-justify-between">
           <div>
             <p className="tw-text-sm tw-text-gray-700">
-              Showing <span className="tw-font-medium">1</span> to <span className="tw-font-medium">1</span> of{' '}
-              <span className="tw-font-medium">1</span> results
+              총 <span className="tw-font-medium">{data?.count}</span>개 리뷰
             </p>
           </div>
           <div>
             <nav className="tw-isolate tw-inline-flex -tw-space-x-px tw-rounded-md tw-shadow" aria-label="Pagination">
-              <div className="tw-relative tw-inline-flex tw-items-center tw-rounded-l-md tw-px-2 tw-py-2 tw-text-gray-400 tw-ring-1 tw-ring-inset tw-ring-gray-300 hover:tw-bg-gray-50 focus:tw-z-20 focus:tw-outline-offset-0 tw-cursor-pointer">
+              <div
+                className="tw-relative tw-inline-flex tw-items-center tw-rounded-l-md tw-px-2 tw-py-2 tw-text-gray-400 tw-ring-1 tw-ring-inset tw-ring-gray-300 hover:tw-bg-gray-50 focus:tw-z-20 focus:tw-outline-offset-0 tw-cursor-pointer">
                 <p>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="tw-w-5 tw-h-5">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                       stroke="currentColor" className="tw-w-5 tw-h-5">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5"/>
                   </svg>
                 </p>
               </div>
-              <div className={selectedPage}>
-                1
-              </div>
-              {/*<div className={notSelectedPage}>*/}
-              {/*  2*/}
-              {/*</div>*/}
-              {/*<div className={notSelectedPage}>*/}
-              {/*  3*/}
-              {/*</div>*/}
-              <div className="tw-relative tw-inline-flex tw-items-center tw-rounded-r-md tw-px-2 tw-py-2 tw-text-gray-400 tw-ring-1 tw-ring-inset tw-ring-gray-300 hover:tw-bg-gray-50 focus:tw-z-20 focus:tw-outline-offset-0 tw-cursor-pointer">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="tw-w-5 tw-h-5">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+              {
+                buttonRender().map(el => (
+                  <div className={el === page.current ? selectedPage : notSelectedPage}
+                       onClick={() => buttonClick(el)}
+                  >
+                    {el}
+                  </div>
+                ))
+              }
+              <div
+                className="tw-relative tw-inline-flex tw-items-center tw-rounded-r-md tw-px-2 tw-py-2 tw-text-gray-400 tw-ring-1 tw-ring-inset tw-ring-gray-300 hover:tw-bg-gray-50 focus:tw-z-20 focus:tw-outline-offset-0 tw-cursor-pointer">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                     stroke="currentColor" className="tw-w-5 tw-h-5">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5"/>
                 </svg>
               </div>
             </nav>
