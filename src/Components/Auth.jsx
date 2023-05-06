2692df37ed81aa204956b54b9854a7ab5a931932
ribeKim <ribe@ribe.io>
2023-05-06 23:03:54 +0900

    request는 백그라운드 워커에서 하도록 수정

--- src/Components/Auth.jsx
+++ src/Components/Auth.jsx
@@ -1,24 +1,6 @@
 function Auth () {
   const Login = () => {
-    chrome.identity.getAuthToken({ interactive: true }, function (token) {
-      console.log(token)
-      let init = {
-        method: 'GET',
-        async: true,
-        headers: {
-          Authorization: 'Bearer ' + token,
-          'Content-Type': 'application/json'
-        },
-        'contentType': 'json'
-      }
-      fetch(
-        'https://people.googleapis.com/v1/contactGroups/all?maxMembers=20&key=API_KEY',
-        init)
-        .then((response) => response.json())
-        .then(function (data) {
-          console.log(data)
-        })
-    })
+    chrome.runtime.sendMessage({ message: 'Auth' })
   }
 
   return (
