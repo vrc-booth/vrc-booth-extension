8be0fa85baed37e2396f0945f1b4447505b59c63
ribeKim <ribe@ribe.io>
2023-05-07 02:18:47 +0900

    web request 태우는 코드 완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import Review from './Components/review.jsx'
-import Heart from './Components/heart.jsx'
 import Pagination from './Components/pagination.jsx'
 import ReviewTextBox from './Components/reviewTextBox.jsx'
+import { useEffect, useState } from 'react'
 
-function App() {
+function App () {
+  const [data, setData] = useState({})
+  const [page, setPage] = useState({ current: 1, pageSize: 5 })
+  const splitUrl = window.location.pathname.split('/')
+
+  useEffect(() => {
+    chrome.runtime.sendMessage({
+      message: 'http',
+      data: {
+        method: 'GET',
+        uri: `/post/${splitUrl[splitUrl.length - 1]}?page=${page.current}&pageSize=${page.pageSize}`
+      }
+    }, response => { setData(response) })
+  }, [page])
+
   return (
     <>
       <div>
         <h3 className="font-bold !m-0 !p-0 typography-16 preserve-half-leading">리뷰</h3>
         <div className="mt-8">
-          <Review/>
-          <Pagination/>
+          <Review list={data.list}/>
+          <Pagination data={data} page={page} onButtonClick={(val) => setPage(val)}/>
           <ReviewTextBox/>
         </div>
       </div>
