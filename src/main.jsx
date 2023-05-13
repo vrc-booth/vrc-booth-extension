499b7a8e88e6af26a9938a7e20568b74ff939437
ribeKim <ribe@ribe.io>
2023-05-13 18:56:55 +0900

    Recoil에 storage 붙여서 사용자 인증 세션으로 들고 있게 만듬

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Auth from './Components/Auth.jsx'
+import { RecoilRoot } from 'recoil'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Auth/>
+    <RecoilRoot>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Auth/>
+      </Suspense>
+    </RecoilRoot>
   </React.StrictMode>,
 )
