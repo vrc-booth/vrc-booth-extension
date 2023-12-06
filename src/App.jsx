93e47383c311b76baa0d85e75be1b4bc66ab5e73
ribeKim <ribe@ribe.io>
2023-12-06 22:43:48 +0900

    modify: 변경한 컴포넌트들 반영

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-import Review from './Components/review.jsx'
-import Pagination from './Components/pagination.jsx'
-import ReviewTextBox from './Components/reviewTextBox.jsx'
-import { useRecoilValue } from 'recoil'
-import { reviewsSelector } from './AppData/selectors.js'
+import CommentTextBox from './components/comment/CommentTextBox.jsx'
+import Auth from './components/auth/Auth.jsx'
+import CommentList from './components/comment/CommentList.jsx'
 
 function App () {
-  const reviewData = useRecoilValue(reviewsSelector)
-
   return (
     <>
       <div>
         <h3 className="font-bold !m-0 !p-0 typography-16 preserve-half-leading">리뷰</h3>
         <div className="mt-8">
-          <Review/>
-          {reviewData.count === 0 || <Pagination/>}
-          <ReviewTextBox/>
+          <CommentList/>
+          <Auth>
+            <CommentTextBox/>
+          </Auth>
         </div>
       </div>
     </>
--- src/Inject.jsx
+++ src/Inject.jsx
@@ -3,21 +3,26 @@ import ReactDOM from 'react-dom/client'
 import { RecoilRoot } from 'recoil'
 import './index.css'
 import App from './App.jsx'
-import Loading from './Loading.jsx'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 const commentRoot = document.querySelector('.u-pt-600')
 const root = document.createElement('div')
 root.id = 'crx-root'
 commentRoot.after(root)
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <>
-      <RecoilRoot>
-        <Suspense fallback={<Loading/>}>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          {/*<Suspense fallback={<Loading/>}>*/}
+          {/*  <App/>*/}
+          {/*</Suspense>*/}
           <App/>
-        </Suspense>
-      </RecoilRoot>
+        </RecoilRoot>
+      </QueryClientProvider>
     </>
   </React.StrictMode>
 )
