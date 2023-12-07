0b81b96860306ad1191e7b19e3029df26608175f
ribeKim <ribe@ribe.io>
2023-12-07 22:48:58 +0900

    add: 크롬 저장소에서 데이터 가져오는 코드

--- src/AppData/configs.js
+++ src/AppData/configs.js
@@ -3,3 +3,46 @@ const BaseURL = process.env.NODE_ENV === 'production' ? 'https://vrc-booth.com/a
 const Configs = {
   BaseURL: BaseURL
 }
+
+const getObjectFromLocalStorage = async function (key) {
+  return new Promise((resolve, reject) => {
+    try {
+      chrome.storage.local.get(key, function (value) {
+        resolve(value[key])
+      })
+    } catch (ex) {
+      reject(ex)
+    }
+  })
+}
+
+const saveObjectInLocalStorage = async function (obj) {
+  return new Promise((resolve, reject) => {
+    try {
+      chrome.storage.local.set(obj, function () {
+        resolve()
+      })
+    } catch (ex) {
+      reject(ex)
+    }
+  })
+}
+
+const removeObjectFromLocalStorage = async function (keys) {
+  return new Promise((resolve, reject) => {
+    try {
+      chrome.storage.local.remove(keys, function () {
+        resolve()
+      })
+    } catch (ex) {
+      reject(ex)
+    }
+  })
+}
+
+export {
+  Configs,
+  getObjectFromLocalStorage,
+  saveObjectInLocalStorage,
+  removeObjectFromLocalStorage
+}
