99530350c4e21e90cd4462e9bdf957ada024108b
ribeKim <ribe@ribe.io>
2023-12-08 01:19:20 +0900

    remove: log 제거

--- src/AppData/ellipseDetail.js
+++ src/AppData/ellipseDetail.js
@@ -3,8 +3,6 @@ const multiItem = document.querySelector('.my-40').children[0]
 const ellipse = document.querySelector('.js-ellip-switcher').cloneNode(true)
 const description = document.querySelector('.main-info-column')
 
-console.log(multiItem)
-
 if (multiItem != null) multiItem.style = 'display: none'
 
 if (description != null) {
