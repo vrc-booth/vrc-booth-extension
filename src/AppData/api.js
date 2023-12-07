f0662f8de39db9ccf566719d2289a51f2bc4643d
ribeKim <ribe@ribe.io>
2023-12-08 01:18:55 +0900

    add: api wrapper

---
+++ src/AppData/api.js
@@ -0,0 +1,56 @@
+import {
+  Configs,
+  getObjectFromLocalStorage,
+  removeObjectFromLocalStorage,
+  saveObjectInLocalStorage
+} from './configs.js'
+
+export const Api = async function (url, options = {}) {
+  const authToken = await getObjectFromLocalStorage('authorization')
+  if (!authToken?.accessToken) throw new Error()
+
+  const defaultOptions = {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${authToken?.accessToken}`
+    },
+  }
+
+  const mergedOptions = {
+    ...defaultOptions,
+    ...options,
+  }
+
+  const response = await fetch(`${Configs.BaseURL}${url}`, mergedOptions)
+
+  if (response.status === 401) {
+    const tokenResponse = await refreshToken({
+      refreshToken: authToken?.refreshToken
+    })
+
+    if (tokenResponse.status === 401) {
+      await removeObjectFromLocalStorage('authorization')
+      return {
+        status: 999,
+        message: 'refreshToken Expired'
+      }
+    }
+
+    saveObjectInLocalStorage({'authorization': tokenResponse.json()})
+      .then(() => {
+        Api(url, mergedOptions)
+      })
+  }
+
+  return response.json()
+}
+
+const refreshToken = async (token) => {
+  return await fetch(`${Configs.BaseURL}/auth/token`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(token)
+  })
+}
