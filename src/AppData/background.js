6c7946eb342f07828d287db0ffe66c0a24a6e884
ribeKim <ribe@ribe.io>
2023-12-07 22:47:58 +0900

    modify: discord oauth 인증 완료

--- src/AppData/background.js
+++ src/AppData/background.js
@@ -1,13 +1,39 @@
 import { Configs } from './configs.js'
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.message === 'auth') {
+const messageMap = {
+  auth: (request, sender, sendResponse) => {
+    const authUrl = new URL(`${Configs.BaseURL}/auth/oauth/discord`)
+    const extensionUrl = new URL(`https://${chrome.runtime.id}.chromiumapp.org/`)
+    authUrl.searchParams.set('redirectUrl', extensionUrl)
     chrome.identity.launchWebAuthFlow({
-        url: `${Configs.BaseURL}/auth/popup/discord`,
+        url: authUrl.toString(),
         interactive: true,
       },
       function (redirectURL) {
-        console.log('OAuth 인증 완료:', redirectURL)
+        const code = new URL(redirectURL).searchParams.get('code')
+        const callbackUrl = new URL(`${Configs.BaseURL}/auth/oauth/discord/callback`)
+        callbackUrl.searchParams.set('code', code)
+        callbackUrl.searchParams.set('redirectUrl', extensionUrl)
+        fetch(callbackUrl.toString())
+          .then(response => response.json())
+          .then(result => {
+            chrome.storage.local.set({ 'authorization': result }, () => {
+              sendResponse(result)
+            })
+          })
+          .catch(error => {
+            console.error('Error during token exchange:', error)
+          })
       })
   }
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (messageMap[request.message]) {
+    messageMap[request.message](request, sender, sendResponse)
+  } else {
+    console.error('Invalid message type: ', request.message)
+  }
+
+  return true
 })
