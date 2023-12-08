00fcf65f758a93fa56908dbf2a4ad20065fb9772
ribeKim <ribe@ribe.io>
2023-12-08 12:09:38 +0900

    modify: 개인 스토어에서 스크립트 작동이 달라서 나눴음

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
 import CommentTextBox from './components/comment/CommentTextBox.jsx'
 import Auth from './components/auth/Auth.jsx'
 import CommentList from './components/comment/CommentList.jsx'
+import React from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { RecoilRoot } from 'recoil'
+
+const queryClient = new QueryClient()
 
 function App () {
   return (
     <>
-      <div>
-        <h3 className="font-bold !m-0 !p-0 typography-16 preserve-half-leading">리뷰</h3>
-        <div className="mt-8">
-          <CommentList/>
-          <Auth>
-            <CommentTextBox/>
-          </Auth>
-        </div>
-      </div>
+      <React.StrictMode>
+        <>
+          <QueryClientProvider client={queryClient}>
+            <RecoilRoot>
+              {/*<Suspense fallback={<Loading/>}>*/}
+              {/*  <App/>*/}
+              {/*</Suspense>*/}
+              <div>
+                <h3 className="font-bold !m-0 !p-0 typography-16 preserve-half-leading">리뷰</h3>
+                <div className="mt-8">
+                  <CommentList/>
+                  <Auth>
+                    <CommentTextBox/>
+                  </Auth>
+                </div>
+              </div>
+            </RecoilRoot>
+          </QueryClientProvider>
+        </>
+      </React.StrictMode>
     </>
   )
 }
