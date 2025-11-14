609303fbdca0b3054e9fd25d670e3680d102c52c
root <root@ribe.localdomain>
2025-11-15 06:41:56 +0900

    chore: use wxt i18n module

--- locales/global.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "*.yml?raw" {
-  const content: string;
-  export default content;
-}
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,18 +1,12 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, WxtViteConfig } from 'wxt';
-import { resolve } from 'node:path';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   vite: (): WxtViteConfig => ({
     plugins: [tailwindcss()],
-    resolve: {
-      alias: {
-        "#i18n": resolve(process.cwd(), "locales/index.ts"),
-      },
-    },
   } as WxtViteConfig),
-  modules: ['@wxt-dev/module-react'],
+  modules: ['@wxt-dev/module-react', '@wxt-dev/i18n/module'],
   manifest: {
     key: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAp7FZB6bFp2sn1uhlBE8K8hYQHsLaAl622sNlZhC/JEnhpKLNpp1DXbbBLyENUheH/yEd5McB3BvOJBNYD2w+Z7BjGvvCcGpBRiOoI/MHyrwqcDKzCdTbu8IQXqcSp2s+JPJhHMYZCwK7kLPV/5aT0x1n2SuizagWNIPeAgNU4vh04OQUXvb6gs/sMLbgH/F8S9ZNU/gOo0A/JabvEBmiMM/KgCTP8tu6m9bNittSdkzWAsheYV3i0MYAv1frhfI/QfoRb4PlP3jf92t4/TRM0/XwFMsVzD8SFRX6fMX/lf3j1tH1LWGJfpHHD0yeOLHsEFAwjJ3pDZcuWsNiL6tENQIDAQAB",
     default_locale: "ko",
